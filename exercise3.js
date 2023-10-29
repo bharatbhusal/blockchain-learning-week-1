e9762eeedc904a89f607be559f8de9931e3f40e1
Bharat Bhusal <bharatbhusal78@gmail.com>
2023-10-29 16:26:39 +0530

    exercise3 done

---
+++ exercise3.js
@@ -0,0 +1,14 @@
+// Exercise 3: Functions
+// Task:
+//   - Write a JavaScript function that takes two numbers as parameters and returns their sum.
+//   - Test the function with different inputs and log the results to the console.
+
+function sum(num1, num2) {
+    return num1 + num2
+}
+
+console.log(`Sum of 1, 2: ${sum(1, 2)}`)
+console.log(`Sum of 1, 20: ${sum(1, 20)}`)
+console.log(`Sum of 11, 24: ${sum(11, 24)}`)
+console.log(`Sum of -1, 2: ${sum(-1, 2)}`)
+console.log(`Sum of 31, 2: ${sum(31, 2)}`)
