db820f804b831716325dd631807bc4363bb72a33
Bharat Bhusal <bharatbhusal78@gmail.com>
2023-10-29 16:11:42 +0530

    exercse one done

---
+++ exercise1.js
@@ -0,0 +1,10 @@
+//Exercise 1: Variables and Data Types
+// Task:
+//   - Declare variables to represent your name, age, and city of residence, and log them to the console.
+//   - Try declaring variables using **`let`**, **`const`**, and **`var`**, and observe the differences.
+
+
+let name = "Bharat Bhusal"
+let age = 22
+const cityOfResidence = "Hyderabad"
+console.log(`My name is ${name}. I am ${age} years old. I am currently living in ${cityOfResidence}.`)
